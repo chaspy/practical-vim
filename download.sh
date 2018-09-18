1b874b553fc495143f4fb46bbebdaa7e8e163ee5
kondo takeshi <chaspy@users.noreply.github.com>
2018-09-18 16:47:33 +0900

    TIP2

--- download.sh
+++ download.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 curl -O http://media.pragprog.com/titles/dnvim/code/the_vim_way/0_mechanics.txt 0_mechanics.txt
+curl -O http://media.pragprog.com/titles/dnvim/code/the_vim_way/2_foo_bar.js 2_foo_bar.js
